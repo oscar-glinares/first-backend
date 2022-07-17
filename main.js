2ea53d1ecdf5f2ccb502b528b8493eacab0a025a
Oscar <theblue154@gmail.com>
2022-07-17 19:35:04 +0200

    Connection database

--- main.js
+++ main.js
@@ -2,17 +2,34 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 const cors = require("cors");
+const mysql = require("mysql");
+const con = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    database:"firstsql"
+})
 
 //middleware
 app.use(express.json()); 
 app.use(cors());
 
+con.connect()
+
 app.listen(PORT, () => console.log("Listening on " + PORT))
 
 app.get("/api/get", (_req, res) => {
-    res.status(200).send("Hello World")
+    con.query("SELECT * FROM users", (err, rows, _fields) => {
+        if(err){
+            throw(err)
+        }
+        res.status(200).send({rows})
+    })
 })
-
-app.post("/api/post/:id",(req, res) => {
-    res.status(200).send("Posted message with id " + req.params.id)
+app.post("/api/post",(req, res) => {
+    con.query(`INSERT INTO users (name, password, email) VALUES ("${req.body.name}","${req.body.password}","${req.body.email}")`, (err) => {
+        if (err){
+            throw(err)
+        }
+        res.status(200).send()
+    })
 })
